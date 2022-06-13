4bc4af3d7549d55776b1a72091dbf205a722e15d
parthasarathibehera <parthasarathi@labglo.com>
2022-06-13 16:32:44 +0530

    packages added into the reuirements file

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class BaseModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
         ordering = ['-created_on']
